ed97c55c4676c06e489860666442b14c63812294
François Sarradin <fsarradin@gmail.com>
2022-01-02 12:09:38 +0100

    add time display

--- src/main/scala/fr/universite_gustave_eiffel/esipe/spark_course/internal/package.scala
+++ src/main/scala/fr/universite_gustave_eiffel/esipe/spark_course/internal/package.scala
@@ -1,11 +1,13 @@
 package fr.universite_gustave_eiffel.esipe.spark_course
 
-import java.io.File
 import scala.util.{Failure, Success, Try}
+
+import java.io.File
+import java.time.LocalDateTime
 
 package object internal {
 
-  private final case class ActivateContext(label: String)
+  final private case class ActivateContext(label: String)
   private var activatedContexts: Seq[ActivateContext] = Seq.empty
 
   def exercise(label: String, activated: Boolean)(f: => Unit): Unit =
@@ -13,12 +15,11 @@ package object internal {
       activatedContexts = activatedContexts :+ ActivateContext(label)
       part(activatedContexts.map(_.label).mkString(" > "))
 
-      try { f } catch {
+      try f
+      catch {
         case e: Exception =>
           throw new PartException(label, e)
-      } finally {
-        activatedContexts = activatedContexts.init
-      }
+      } finally activatedContexts = activatedContexts.init
     } else ()
 
   def exercise(label: String)(f: => Unit): Unit = exercise(label, activated = true)(f)
@@ -38,8 +39,10 @@ package object internal {
         case Failure(e)     => s"${Console.RED}ERROR: ${e.getClass.getCanonicalName}: ${e.getMessage}${Console.RESET}"
       }
 
-    println(("\t" * activatedContexts.size) + s">>> $label: $display")
+    println(partIndent + s">>> $label: $display")
   }
+
+  private def partIndent: String = "\t" * activatedContexts.size
 
   def clean(outputFilename: String): Unit = {
     val file = new File(outputFilename)
@@ -51,6 +54,48 @@ package object internal {
       } else {
         file.delete()
       }
+    }
+  }
+
+  def time[A](label: String)(f: => A): A = {
+    val start = java.lang.System.nanoTime()
+    println(partIndent + s""">>> ${Console.YELLOW}Start "$label" @ ${LocalDateTime.now}${Console.RESET}""")
+    try f
+    finally {
+      val end = java.lang.System.nanoTime()
+      println(partIndent + s""">>> ${Console.YELLOW}End "$label" @ ${LocalDateTime.now}${Console.RESET}""")
+      val delta = (end - start) * 1e-9
+      println(
+        partIndent + s""">>> ${Console.YELLOW}Elapsed time for "$label": ${Console.RED}${displayTime(
+          delta
+        )}${Console.RESET}"""
+      )
+    }
+  }
+
+  private def displayTime(seconds: Double): String = {
+    def round2dec(value: Double): Double = Math.round(value * 100.0) / 100.0
+
+    val posSeconds = Math.abs(seconds)
+    lazy val order = Math.floor(Math.log10(posSeconds))
+    if (posSeconds < 1e-9) "0s"
+    else if (posSeconds >= 60.0 * 60.0) {
+      val hour       = (seconds / (60.0 * 60.0)).toInt
+      val minutes    = ((seconds / 60.0) - hour * 60.0).toInt
+      val remSeconds = round2dec((seconds / 60.0 - (hour * 60.0 + minutes)) * 60.0)
+      s"${hour}h${minutes}m${remSeconds}s"
+    } else if (posSeconds >= 60.0 && posSeconds < 60.0 * 60.0) {
+      val minutes    = (seconds / 60.0).toInt
+      val remSeconds = round2dec((seconds / 60.0 - minutes) * 60.0)
+      s"${minutes}m${remSeconds}s"
+    } else if (order < 0 && order >= -3.0) {
+      s"${round2dec(seconds * 1e3)}ms"
+    } else if (order < -3 && order >= -6.0) {
+      s"${round2dec(seconds * 1e6)}µs"
+    } else if (order < -6 && order >= -9.0) {
+      s"${round2dec(seconds * 1e9)}ns"
+    } else {
+      s"${round2dec(seconds)}s"
     }
   }
 
